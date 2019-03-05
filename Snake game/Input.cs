d358d90a59cfcc2c6db9802c8a75682d734ac5d5
Ionut-Victor Diaconu <diaconu.ionut96@gmail.com>
2019-03-05 10:53:54 +0200

    SnakeGame

---
+++ Snake game/Input.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_game
+{
+    class Input
+    {
+        private static Hashtable keyTable = new Hashtable();
+
+        public static bool KeyPress(Keys key)
+        {
+            //Return a key back to the class
+            if (keyTable[key] == null)
+            {
+                return false;
+            }
+            return (bool)keyTable[key];
+        }
+
+        public static void ChangeState(Keys key, bool state)
+        {
+            keyTable[key] = state;
+        }
+    }
+}
